2365e446e0b2f4444311c48ea9061678b39aaa91
Gwen <g@10degres.net>
2019-12-12 18:39:44 +0100

    fix urls &endpoints

--- myrecon.py
+++ myrecon.py
@@ -17,10 +17,10 @@ config = {
     },
     'endpoints': {
         'commands': [
-            # "'github-endpoints.py -d ' + domain + ' -s | tee -a raw_github-endpoints endpoints_grabbed 2>&1 >/dev/null &'",
+            "'github-endpoints.py -d ' + domain + ' -s | tee -a raw_github-endpoints endpoints_grabbed 2>&1 >/dev/null &'",
             "'google-search.py -d -e 100 -t \"site:' + domain + '\" | tee -a raw_google-domain endpoints_grabbed 2>&1 >/dev/null &'",
-            # "'google-search.py -d -e 100 -t \"site:' + domain + ' inurl:&\" | tee -a raw_google-domain endpoints_grabbed 2>&1 >/dev/null &'",
-            # "'waybackurls ' + domain + ' true ' + os.getcwd() + '/raw_wayback 2>&1 >/dev/null &'",
+            "'google-search.py -d -e 100 -t \"site:' + domain + ' inurl:&\" | tee -a raw_google-domain endpoints_grabbed 2>&1 >/dev/null &'",
+            "'waybackurls ' + domain + ' true ' + os.getcwd() + '/raw_wayback 2>&1 >/dev/null &'",
         ]
     },
     'ishttp': {
